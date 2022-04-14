3d235250f8c850adbefa65d266e3033823f7a7bb
Jason D <53819227+jd-launch-school@users.noreply.github.com>
2022-04-14 07:54:25 -0400

    Add feelings detection

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,4 @@
-const responses = [
+const idesResponses = [
   'Great idea! Thank you for sharing.',
   "I don't agree, but I appreciate you sharing.",
   "Your ideas are valid, but I didn't ask for your ideas.",
@@ -8,12 +8,13 @@ const responses = [
   'You may be missing some things, but I can see your main point.',
   'I hear you. Thank you for sharing.',
   "You're right. That's also a way I could have looked at the situation.",
-  'OK, thank you.', 'I like that idea. Thanks for sharing.',
+  'OK, thank you.',
+  'I like that idea. Thanks for sharing.',
   "I'm not a fan of that idea, but I appreciate you sharing.",
   "That's interesting! I hadn't thought of it that way.",
   "Um, sorry, you missed my point. I appreciate your idea but also feel like you didn't listen to me.",
   "That seems sound. I don't have any more time or energy to talk about this right now.",
-  "Appreciate that. I'm mostly venting, so I don't want to talk about this that deep.",
+  "Appreciate that. I'm mostly venting, so I don't want to talk about this that deeply.",
   "Sure, that too. It's fine to have that view. I don't want to talk more right now because I feel like I'm being attacked.",
   "That sounds right. I'm busy with other things right now so can we talk about this later?",
   'Thanks. I appreciate you sharing. However, I already know about this.',
@@ -21,8 +22,17 @@ const responses = [
   'Thank you. I appreciate that. It really could be anything, so no worries.'
 ];
 
+feelingResponses = [
+  "I'm sorry you feel that way. That's not my intention.",
+  "Thank you for sharing. I'm not doing this intentionally. I also feel that way about you.",
+  'Your feelings are valid and important. I think you may be misunderstanding the situation. Can I help get us on the same page?',
+  'Thanks for sharing. I love you and I will do what I can to help.',
+  "I understand that you feel this way. I believe you. However, it feels like you're attacking me so I need to take a break to think about this.",
+];
+
 // TODO - add emojis and positive stuff
 // TODO - All good? button or system
+// TODO - Pick a different response?
 
 window.onload = function () {
   document.querySelector('textarea').value = '';
@@ -45,9 +55,17 @@ async function validatorinator (event) {
   // const responseText = document.querySelector('#response-text');
   const hiddenElements = document.querySelectorAll('.hidden');
   const responseSection = document.querySelector('#response-section');
-
-  const randomNum = Math.floor(Math.random() * (responses.length));
-  const response = responses[randomNum];
+  
+  const feelingWords = ['I feel', "I'm feeling", 'makes me feel'];
+  const feelingInIdeaText = feelingWords.some(word => ideaText.includes(word));
+  
+  if (feelingInIdeaText) {
+    randomNum = Math.floor(Math.random() * (feelingResponses.length));
+    response = feelingResponses[randomNum];
+  } else {
+    const randomNum = Math.floor(Math.random() * (ideaResponses.length));
+    response = ideaResponses[randomNum];
+  }
 
   form.style.display = 'none';
 
